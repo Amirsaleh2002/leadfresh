b4939346a30a9b12e3131ef412b2d4a489d8db7d
saleh rezaei <amsaleh.r81@gmail.com>
2026-09-04 20:07:52 +0330

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "standalone",
-  images: {
-    remotePatterns: [],
-    dangerouslyAllowSVG: true,
-    deviceSizes: [320, 420, 768, 1024, 1200, 1600, 1920, 2560],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    unoptimized: true,
-    loader: "default",
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
